c0954ae65a46219e4842e1ff709e90a237d0497f
wangfukang <wangfukang@kuaishou.com>
2021-11-24 11:33:23 +0800

    first commit

---
+++ src/components/Child.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ childProps.a }}</div>
+</template>
+
+<script>
+export default {
+  name: "child",
+  props: {
+    childProps: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  watch: {
+    childProps: {
+      handler(val, oldVal) {
+        console.log("child props childProps update");
+      },
+      deep: true,
+    },
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <child :child-props="infos.childProps || {}" />
+    <button @click="btnClick">add a field in item</button>
+  </div>
+</template>
+
+<script>
+import Child from "@/components/Child.vue"; // @ is an alias to /src
+
+export default {
+  name: "parent",
+  components: {
+    Child,
+  },
+  data: function () {
+    return {
+      item: {
+        name: "wfk",
+        infos: {
+          a: 111,
+          b: 222,
+          c: 333,
+          childProps: {
+            a: 1,
+            b: 2,
+            c: 3,
+          },
+        },
+      },
+    };
+  },
+  methods: {
+    btnClick() {
+      this.$set(this.item, "age", 18);
+    },
+  },
+  computed: {
+    infos: function () {
+      return this.item.infos || {};
+    },
+  },
+  watch: {
+    infos: function (newQuestion, oldQuestion) {
+      console.log("parent infos update");
+    },
+    "infos.childProps": function (newQuestion, oldQuestion) {
+      console.log("parent infos.childProps update");
+    },
+  },
+};
+</script>
